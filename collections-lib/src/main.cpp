929ca552ca03b28c29824ccbd94766da734ada33
romango78 <romango78@gmail.com>
2021-10-25 23:30:16 +0300

    Added collections-lib

---
+++ collections-lib/src/main.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Roman Gorielov. All Rights Reserved.
+// 
+// This software is the confidential and proprietary information of Roman Gorielov.
+// It is furnished under license and may only be used or copied in accordance
+// with the terms of such license.
+// This software is subject to change without notice and no information
+// contained in it should be construed as commitment by Roman Gorielov.
+
+#include <Arduino.h>
+
+void setup() 
+{
+    // put your setup code here, to run once:
+}
+
+void loop() 
+{
+    // put your main code here, to run repeatedly:
+    delay(100);
+}
