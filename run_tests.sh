317fb42808dbd62bb6b0d4d6249509b444f8fe8d
Roman Gorielov <roman_gorielov@epam.com>
2024-03-29 10:50:52 +0200

    test

--- run_tests.sh
+++ run_tests.sh
@@ -34,7 +34,8 @@ declare projectfailed=0
 # Initialize the counters
 declare -A counts=(["Tests"]=0 ["Failures"]=0 ["Ignored"]=0)
 
-counts[0]=20
+counts["Tests"]=20
+counts["Tests"]=+10
 
 # Define the AWK command to increment the correct counter for matching lines
 awkcmd='
@@ -67,7 +68,7 @@ for folder in $(find . -name "test" -type d -print); do
 
 
 
-        echo ""
+        echo " "
 
     fi
     break
